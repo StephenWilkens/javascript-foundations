90ac654c1efe43684f191f882e967db3ebcbcc11
Stephen Wilkens <stephenwilkens@gmail.com>
2022-10-16 17:24:48 -0600

    Dragon -1

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,25 @@
+class Dragon {
+    constructor(name, rider) {
+        this.name = name
+        this.rider = rider
+        this.eatCount = 0
+        if (this.eatCount >= 3) {
+            this.hungry = false;
+        }
+        else {
+            this.hungry = true;
+        }
+    }
 
+    greet() {
+        return `Hi, ${this.rider}!`
+    }
+
+    eat() {
+        this.eatCount += 1
+        console.log(this.eatCount)
+        console.log(this.hungry)
+    }
+}
+
+module.exports = Dragon
