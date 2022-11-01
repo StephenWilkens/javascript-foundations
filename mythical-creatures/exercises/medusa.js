cd59b924b4f39386788849011b80ffa846d54dbb
StephenWilkens <stephenwilkens@gmail.com>
2022-11-01 11:38:32 -0600

    Initial commit

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,15 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name
+    this.statues = []
+  }
+
+  gazeAtVictim(victim) {
+    this.statues.push(victim)
+  }
+}
+
+module.exports = Medusa
